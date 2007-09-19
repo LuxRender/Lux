2668a7d298ad302cbf6f75ab18d4fb5dd67dbe3a
jromang <devnull@localhost>
2007-09-19 20:08:25 +0200

    Upgraded copyright notices to GPL V3.

--- shapes/hyperboloid.cpp
+++ shapes/hyperboloid.cpp
@@ -1,12 +1,24 @@
-
-/*
- * pbrt source code Copyright(c) 1998-2007 Matt Pharr and Greg Humphreys
- *
- * All Rights Reserved.
- * For educational use only; commercial use expressly forbidden.
- * NO WARRANTY, express or implied, for this software.
- * (See file License.txt for complete license)
- */
+/***************************************************************************
+ *   Copyright (C) 1998-2007 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of Lux Renderer.                                    *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.org                       *
+ ***************************************************************************/
 
 // hyperboloid.cpp*
 #include "shape.h"
