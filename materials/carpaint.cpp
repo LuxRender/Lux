d11ed1d75f8f012edbd2c1b50ccf3f1c37defc8c
tomb <devnull@localhost>
2007-11-02 23:18:14 +0100

    Changed carpaint to accept named presets

--- materials/carpaint.cpp
+++ materials/carpaint.cpp
@@ -106,28 +106,106 @@ BSDF *CarPaint::GetBSDF(MemoryArena &arena, const DifferentialGeometry &dgGeom, 
   return bsdf;
 }
 
+void DataFromName(const string name, boost::shared_ptr<Texture<Spectrum> > *Kd,
+                                     boost::shared_ptr<Texture<Spectrum> > *Ks1,
+                                     boost::shared_ptr<Texture<Spectrum> > *Ks2,
+                                     boost::shared_ptr<Texture<Spectrum> > *Ks3,
+                                     boost::shared_ptr<Texture<float> > *R1,
+                                     boost::shared_ptr<Texture<float> > *R2,
+                                     boost::shared_ptr<Texture<float> > *R3,
+                                     boost::shared_ptr<Texture<float> > *M1,
+                                     boost::shared_ptr<Texture<float> > *M2,
+                                     boost::shared_ptr<Texture<float> > *M3) {
+
+  int numPaints = sizeof(carpaintdata) / sizeof(CarPaintData);
+
+  // default (Ford F8)
+
+  int i;
+
+  for (i = 0; i < numPaints; i++) {
+    if (name.compare(carpaintdata[i].name) == 0)
+      break;
+  }
+
+  boost::shared_ptr<Texture<Spectrum> > kd (new ConstantTexture<Spectrum>(carpaintdata[i].kd));
+  boost::shared_ptr<Texture<Spectrum> > ks1 (new ConstantTexture<Spectrum>(carpaintdata[i].ks1));
+  boost::shared_ptr<Texture<Spectrum> > ks2 (new ConstantTexture<Spectrum>(carpaintdata[i].ks2));
+  boost::shared_ptr<Texture<Spectrum> > ks3 (new ConstantTexture<Spectrum>(carpaintdata[i].ks3));
+  boost::shared_ptr<Texture<float> > r1 (new ConstantTexture<float>(carpaintdata[i].r1));
+  boost::shared_ptr<Texture<float> > r2 (new ConstantTexture<float>(carpaintdata[i].r2));
+  boost::shared_ptr<Texture<float> > r3 (new ConstantTexture<float>(carpaintdata[i].r3));
+  boost::shared_ptr<Texture<float> > m1 (new ConstantTexture<float>(carpaintdata[i].m1));
+  boost::shared_ptr<Texture<float> > m2 (new ConstantTexture<float>(carpaintdata[i].m2));
+  boost::shared_ptr<Texture<float> > m3 (new ConstantTexture<float>(carpaintdata[i].m3));
+
+  *Kd = kd;
+  *Ks1 = ks1;
+  *Ks2 = ks2;
+  *Ks3 = ks3;
+  *R1 = r1;
+  *R2 = r2;
+  *R3 = r3;
+  *M1 = m1;
+  *M2 = m2;
+  *M3 = m3;
+}
+
 Material* CarPaint::CreateMaterial(const Transform &xform, const TextureParams &mp) {
-  // The default values are for "Polaris Silver"
-  float def_kd[3] = {0.055f, 0.063f, 0.071f};
-  float def_ks1[3] = {0.065f, 0.082f, 0.088f};
-  float def_ks2[3] = {0.11f, 0.11f, 0.13f};
-  float def_ks3[3] = {0.008f, 0.013f, 0.015f};
-  float def_r[3] = {1.0f, 0.92f, 0.9f};
-  float def_m[3] = {0.38f, 0.17f, 0.013f};
 
-  boost::shared_ptr<Texture<Spectrum> > Kd = mp.GetSpectrumTexture("Kd", Spectrum(def_kd));
+  // Default values for missing parameters is from the Ford F8 dataset
+  float def_kd[3], def_ks1[3], def_ks2[3], def_ks3[3], def_r[3], def_m[3];
 
-  boost::shared_ptr<Texture<Spectrum> > Ks1 = mp.GetSpectrumTexture("Ks1", Spectrum(def_ks1));
-  boost::shared_ptr<Texture<Spectrum> > Ks2 = mp.GetSpectrumTexture("Ks2", Spectrum(def_ks2));
-  boost::shared_ptr<Texture<Spectrum> > Ks3 = mp.GetSpectrumTexture("Ks3", Spectrum(def_ks3));
+  for (int i = 0; i < 3; i++) {
+    def_kd[i] = carpaintdata[0].kd[i];
+    def_ks1[i] = carpaintdata[0].ks1[i];
+    def_ks2[i] = carpaintdata[0].ks2[i];
+    def_ks3[i] = carpaintdata[0].ks3[i];
+  }
 
-  boost::shared_ptr<Texture<float> > R1 = mp.GetFloatTexture("R1", def_r[0]);
-  boost::shared_ptr<Texture<float> > R2 = mp.GetFloatTexture("R2", def_r[1]);
-  boost::shared_ptr<Texture<float> > R3 = mp.GetFloatTexture("R3", def_r[2]);
+  def_r[0] = carpaintdata[0].r1;
+  def_r[1] = carpaintdata[0].r2;
+  def_r[2] = carpaintdata[0].r3;
 
-  boost::shared_ptr<Texture<float> > M1 = mp.GetFloatTexture("M1", def_m[0]);
-  boost::shared_ptr<Texture<float> > M2 = mp.GetFloatTexture("M2", def_m[1]);
-  boost::shared_ptr<Texture<float> > M3 = mp.GetFloatTexture("M3", def_m[2]);
+  def_m[0] = carpaintdata[0].m1;
+  def_m[1] = carpaintdata[0].m2;
+  def_m[2] = carpaintdata[0].m3;
+
+  string paintname = mp.FindString("name");
+
+  boost::shared_ptr<Texture<Spectrum> > Kd;
+
+  boost::shared_ptr<Texture<Spectrum> > Ks1;
+  boost::shared_ptr<Texture<Spectrum> > Ks2;
+  boost::shared_ptr<Texture<Spectrum> > Ks3;
+
+  boost::shared_ptr<Texture<float> > R1;
+  boost::shared_ptr<Texture<float> > R2;
+  boost::shared_ptr<Texture<float> > R3;
+
+  boost::shared_ptr<Texture<float> > M1;
+  boost::shared_ptr<Texture<float> > M2;
+  boost::shared_ptr<Texture<float> > M3;
+
+  if (paintname.length() < 1) {
+    // we got no name, so try to read material properties directly
+    Kd = mp.GetSpectrumTexture("Kd", Spectrum(def_kd));
+
+    Ks1 = mp.GetSpectrumTexture("Ks1", Spectrum(def_ks1));
+    Ks2 = mp.GetSpectrumTexture("Ks2", Spectrum(def_ks2));
+    Ks3 = mp.GetSpectrumTexture("Ks3", Spectrum(def_ks3));
+
+    R1 = mp.GetFloatTexture("R1", def_r[0]);
+    R2 = mp.GetFloatTexture("R2", def_r[1]);
+    R3 = mp.GetFloatTexture("R3", def_r[2]);
+
+    M1 = mp.GetFloatTexture("M1", def_m[0]);
+    M2 = mp.GetFloatTexture("M2", def_m[1]);
+    M3 = mp.GetFloatTexture("M3", def_m[2]);
+  }
+  else
+    // Pick from presets, fall back to the first if name not found
+    DataFromName(paintname, &Kd, &Ks1, &Ks2, &Ks3, &R1, &R2, &R3, &M1, &M2, &M3);
 
   boost::shared_ptr<Texture<float> > bumpMap = mp.GetFloatTexture("bumpmap", 0.f);
 
