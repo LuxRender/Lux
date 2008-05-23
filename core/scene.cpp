e8ba97aa0b9b4eb2a2cd7427933755492f134760
dade916 <devnull@localhost>
2008-05-23 22:27:41 +0200

    Network rendering server now avoids to save resume film

--- core/scene.cpp
+++ core/scene.cpp
@@ -337,6 +337,7 @@ void RenderThread::render(RenderThread *myThread) {
 }
 
 int Scene::CreateRenderThread() {
+    // Dade - this code is not thread safe
     RenderThread *rt = new  RenderThread(renderThreads.size(),
             CurThreadSignal,
             surfaceIntegrator,
--- renderer/renderserver.cpp
+++ renderer/renderserver.cpp
@@ -143,11 +143,13 @@ static void processCommandFilm(void (&f)(const string &, const ParamSet &), basi
     params.EraseBool("write_untonemapped_exr");
     params.EraseBool("write_untonemapped_igi");
     params.EraseBool("write_tonemapped_tga");
+    params.EraseBool("write_resume_flm");
 
     params.AddBool("write_tonemapped_exr", new bool(false));
     params.AddBool("write_untonemapped_exr", new bool(false));
     params.AddBool("write_untonemapped_igi", new bool(false));
     params.AddBool("write_tonemapped_tga", new bool(false));
+    params.AddBool("write_resume_flm", new bool(false));
 
     f(type.c_str(), params);
 }
