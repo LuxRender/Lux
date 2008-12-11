c9529f368fbd251c3f4c5303d626dd32e917a99c
jeanphi <devnull@localhost>
2008-12-11 20:42:31 +0100

    Add support for spot lights to bidirectional.
    Jeanphi

--- lights/spot.cpp
+++ lights/spot.cpp
@@ -26,9 +26,48 @@
 #include "spd.h"
 #include "rgbillum.h"
 #include "paramset.h"
+#include "reflection/bxdf.h"
 #include "dynload.h"
 
 using namespace lux;
+
+static float LocalFalloff(const Vector &w, float cosTotalWidth, float cosFalloffStart)
+{
+	if (CosTheta(w) < cosTotalWidth)
+		return 0.f;
+ 	if (CosTheta(w) > cosFalloffStart)
+		return 1.f;
+	// Compute falloff inside spotlight cone
+	const float delta = (CosTheta(w) - cosTotalWidth) /
+		(cosFalloffStart - cosTotalWidth);
+	return powf(delta, 4);
+}
+class SpotBxDF : public BxDF
+{
+public:
+	SpotBxDF(float width, float fall) : BxDF(BxDFType(BSDF_REFLECTION | BSDF_DIFFUSE)), cosTotalWidth(width), cosFalloffStart(fall) {}
+	void f(const TsPack *tspack, const Vector &wo, const Vector &wi, SWCSpectrum *const f) const {
+		*f += LocalFalloff(wi, cosTotalWidth, cosFalloffStart);
+	}
+	bool Sample_f(const TsPack *tspack, const Vector &wo, Vector *wi, float u1, float u2, SWCSpectrum *const f,float *pdf, float *pdfBack = NULL, bool reverse = false) const
+	{
+		*wi = UniformSampleCone(u1, u2, cosTotalWidth);
+		*pdf = UniformConePdf(cosTotalWidth);
+		if (pdfBack)
+			*pdfBack = Pdf(*wi, wo);
+		*f = LocalFalloff(*wi, cosTotalWidth, cosFalloffStart);
+		return true;
+	}
+	float Pdf(const Vector &wi, const Vector &wo) const
+	{
+		if (CosTheta(wo) < cosTotalWidth)
+			return 0.f;
+		else
+			return UniformConePdf(cosTotalWidth);
+	}
+private:
+	float cosTotalWidth, cosFalloffStart;
+};
 
 // SpotLight Method Definitions
 SpotLight::SpotLight(const Transform &light2world,
@@ -48,16 +87,7 @@ SpotLight::~SpotLight()
 	delete LSPD;
 }
 float SpotLight::Falloff(const Vector &w) const {
-	Vector wl = Normalize(WorldToLight(w));
-	float costheta = wl.z;
-	if (costheta < cosTotalWidth)
-		return 0.;
- 	if (costheta > cosFalloffStart)
-		return 1.;
-	// Compute falloff inside spotlight cone
-	float delta = (costheta - cosTotalWidth) /
-		(cosFalloffStart - cosTotalWidth);
-	return delta*delta*delta*delta;
+	return LocalFalloff(Normalize(WorldToLight(w)), cosTotalWidth, cosFalloffStart);
 }
 SWCSpectrum SpotLight::Sample_L(const TsPack *tspack, const Point &p, float u1, float u2, float u3,
 		Vector *wi, float *pdf, VisibilityTester *visibility) const {
@@ -78,6 +108,40 @@ SWCSpectrum SpotLight::Sample_L(const TsPack *tspack, const Scene *scene, float 
 	ray->d = LightToWorld(v);
 	*pdf = UniformConePdf(cosTotalWidth);
 	return SWCSpectrum(tspack, LSPD) * Falloff(ray->d);
+}
+bool SpotLight::Sample_L(const TsPack *tspack, const Scene *scene, float u1, float u2, float u3, BSDF **bsdf, float *pdf, SWCSpectrum *Le) const
+{
+	Normal ns = LightToWorld(Normal(0, 0, 1));
+	Vector dpdu, dpdv;
+	CoordinateSystem(Vector(ns), &dpdu, &dpdv);
+	DifferentialGeometry dg(lightPos, ns, dpdu, dpdv, Vector(0, 0, 0), Vector(0, 0, 0), 0, 0, NULL);
+	*bsdf = BSDF_ALLOC(tspack, BSDF)(dg, ns);
+	(*bsdf)->Add(BSDF_ALLOC(tspack, SpotBxDF)(cosTotalWidth, cosFalloffStart));
+	*pdf = 1.f;
+	*Le = SWCSpectrum(tspack, LSPD);
+	return true;
+}
+bool SpotLight::Sample_L(const TsPack *tspack, const Scene *scene, const Point &p, const Normal &n,
+	float u1, float u2, float u3, BSDF **bsdf, float *pdf, float *pdfDirect,
+	VisibilityTester *visibility, SWCSpectrum *Le) const
+{
+	const Vector w(p - lightPos);
+	*pdfDirect = 1.f / w.LengthSquared();
+	Normal ns = LightToWorld(Normal(0, 0, 1));
+	*pdf = 1.f;
+	Vector dpdu, dpdv;
+	CoordinateSystem(Vector(ns), &dpdu, &dpdv);
+	DifferentialGeometry dg(lightPos, ns, dpdu, dpdv, Vector(0, 0, 0), Vector(0, 0, 0), 0, 0, NULL);
+	*bsdf = BSDF_ALLOC(tspack, BSDF)(dg, ns);
+	(*bsdf)->Add(BSDF_ALLOC(tspack, SpotBxDF)(cosTotalWidth, cosFalloffStart));
+	visibility->SetSegment(p, lightPos);
+	*Le = SWCSpectrum(tspack, LSPD);
+	return true;
+}
+SWCSpectrum SpotLight::Le(const TsPack *tspack, const Scene *scene, const Ray &r,
+	const Normal &n, BSDF **bsdf, float *pdf, float *pdfDirect) const
+{
+	return SWCSpectrum(0.f);
 }
 Light* SpotLight::CreateLight(const Transform &l2w, const ParamSet &paramSet) {
 	RGBColor I = paramSet.FindOneRGBColor("I", RGBColor(1.0));
