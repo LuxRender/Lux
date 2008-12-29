fb4a8f90a8b5cb9c7fb071d3f346c87ad5604591
lordcrc <devnull@localhost>
2008-12-29 03:57:41 +0100

    Made toMatrix() return a proper 4x4 matrix (ie fill in the fourth row & colum)

--- core/geometry/quaternion.cpp
+++ core/geometry/quaternion.cpp
@@ -122,6 +122,11 @@ void toMatrix(const Quaternion &q, float m[4][4]) {
   m[2][0] = -m[2][0];
   m[2][1] = -m[2][1];
   m[2][2] = -m[2][2];
+
+  // complete matrix
+  m[0][3] = m[1][3] = m[2][3] = 0;
+  m[3][0] =  m[3][1] = m[3][2] = 0;
+  m[3][3] = 1;
 }
 
 
