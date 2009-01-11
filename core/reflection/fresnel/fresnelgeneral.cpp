5b4651074c5b235af36713c37b227f1529f80925
jeanphi <devnull@localhost>
2009-01-11 21:06:37 +0100

    New fresnel primitive for general purpose usage.
    Jeanphi

---
+++ core/reflection/fresnel/fresnelgeneral.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2008 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// fresnelgeneral.cpp*
+#include "fresnelgeneral.h"
+#include "spectrum.h"
+
+using namespace lux;
+
+void FresnelGeneral::Evaluate(const TsPack *tspack, float cosi, SWCSpectrum *const f) const {
+	SWCSpectrum cost(sqrtf(max(0.f, 1.f - cosi * cosi)));
+	if (cosi > 0.f)
+		cost = cost / (eta * eta + k * k).Sqrt();
+	else
+		cost = cost * (eta * eta + k * k).Sqrt();
+	cost.Clamp(0.f, 1.f);
+	cost = (SWCSpectrum(1.f) - cost * cost).Sqrt();
+	FrFull(fabsf(cosi), cost, eta, k, f);
+}
---
+++ core/reflection/fresnel/fresnelgeneral.h
@@ -0,0 +1,52 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2008 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+#ifndef LUX_FRESNELGENERAL_H
+#define LUX_FRESNELGENERAL_H
+// fresnelgeneral.h*
+#include "lux.h"
+#include "fresnel.h"
+
+namespace lux
+{
+
+class  FresnelGeneral : public Fresnel {
+public:
+	// FresnelGeneral Public Methods
+	void Evaluate(const TsPack *tspack, float cosi, SWCSpectrum *const f) const;
+	FresnelGeneral(const SWCSpectrum &e, const SWCSpectrum &kk)
+		: eta(e), k(kk) {
+	}
+	FresnelGeneral(const SWCSpectrum &ei, const SWCSpectrum &ki, const SWCSpectrum &et, const SWCSpectrum &kt) {
+		SWCSpectrum norm(ei * ei + ki * ki);
+		eta = (ei * et + ki * kt) / norm;
+		k = (ei * kt - et * ki) / norm;
+	}
+private:
+	// FresnelGeneral Private Data
+	SWCSpectrum eta, k;
+};
+
+}//namespace lux
+
+#endif // LUX_FRESNELCONDUCTOR_H
+
