112d347a53fd7e8d87dfc9fa15e3e593e4f1c017
jeanphi <devnull@localhost>
2009-01-12 21:34:59 +0100

    New linear tonemapping.
    Jeanphi

---
+++ tonemaps/lineartonemap.cpp
@@ -0,0 +1,38 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2008 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// lineartonemap.cpp*
+#include "lineartonemap.h"
+#include "paramset.h"
+#include "dynload.h"
+
+using namespace lux;
+
+// MaxWhiteOp Method Definitions
+ToneMap * LinearOp::CreateToneMap(const ParamSet &ps) {
+	float sensitivity = ps.FindOneFloat("sensitivity", 100.f);
+	float exposure = ps.FindOneFloat("exposure", 1.f / 1000.f);
+	float fstop = ps.FindOneFloat("fstop", 2.8f);
+	return new LinearOp(sensitivity, exposure, fstop);
+}
+
+static DynamicLoader::RegisterToneMap<LinearOp> r("linear");
---
+++ tonemaps/lineartonemap.h
@@ -0,0 +1,49 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2008 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+// lineartonemap.h*
+#include "lux.h"
+#include "tonemap.h"
+#include "color.h"
+
+namespace lux
+{
+
+// MaxWhiteOp Declarations
+class LinearOp : public ToneMap {
+public:
+	// LinearOp Public Methods
+	LinearOp(float sensitivity, float exposure, float fstop) : factor(exposure / (fstop * fstop) * sensitivity / 10.f * 118.f / 25.f) { }
+	void Map(vector<Color> &xyz, int xRes, int yRes, float maxDisplayY) const {
+		const int numPixels = xRes * yRes;
+		for (int i = 0; i < numPixels; ++i)
+			xyz[i] *= factor;
+	}
+	
+	static ToneMap *CreateToneMap(const ParamSet &ps);
+private:
+	// LinearOp Private Data
+	const float factor;
+};
+
+}//namespace lux
+
