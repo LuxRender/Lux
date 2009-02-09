7bdfba222fb47d8b039b13ac9974b40e9b4d5a57
jeanphi <devnull@localhost>
2009-02-09 23:40:45 +0100

    Sky optimizations (after silly typo fix).
    Jeanphi

--- lights/sky.h
+++ lights/sky.h
@@ -75,7 +75,6 @@ public:
 
 	void		InitSunThetaPhi();
 	void		ChromaticityToSpectrum(const TsPack *tspack, const float x, const float y, SWCSpectrum * const dst_spect) const;
-	float		PerezFunction(const float *lam, float theta, float phi, float lvz) const;
 
 private:
 	// SkyLight Private Data
