b7e7f2f8478069794534425254393d43ade8132b
David Bucciarelli <dade@ngi.it>
2009-10-05 11:08:46 +0200

    More development on Luxcomp

---
+++ tools/luxcomp.cpp
@@ -0,0 +1,354 @@
+/***************************************************************************
+ *   Copyright (C) 1998-2009 by authors (see AUTHORS.txt )                 *
+ *                                                                         *
+ *   This file is part of LuxRender.                                       *
+ *                                                                         *
+ *   Lux Renderer is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   Lux Renderer is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ *   This project is based on PBRT ; see http://www.pbrt.org               *
+ *   Lux Renderer website : http://www.luxrender.net                       *
+ ***************************************************************************/
+
+#include <iomanip>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <exception>
+#include <iostream>
+
+#include <boost/program_options.hpp>
+#include <boost/thread.hpp>
+#include <boost/thread/xtime.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <boost/filesystem/path.hpp>
+#include <boost/filesystem/operations.hpp>
+#include <boost/scoped_ptr.hpp>
+
+#include "lux.h"
+#include "api.h"
+#include "context.h"
+#include "fleximage.h"
+#include "error.h"
+#include "osfunc.h"
+
+#if defined(WIN32) && !defined(__CYGWIN__) /* We need the following two to set stdout to binary */
+#include <io.h>
+#include <fcntl.h>
+#endif
+
+#if defined(WIN32) && !defined(__CYGWIN__)
+#include "direct.h"
+#define chdir _chdir
+#endif
+
+using namespace lux;
+namespace po = boost::program_options;
+
+void CheckFilePath(const std::string fileName) {
+	boost::filesystem::path fullPath(boost::filesystem::initial_path());
+	fullPath = boost::filesystem::system_complete(boost::filesystem::path(
+			fileName, boost::filesystem::native));
+
+	if (!boost::filesystem::exists(fullPath)) {
+		std::stringstream ss;
+		ss << "Unable to open file '" << fullPath.string() << "'";
+		luxError(LUX_NOFILE, LUX_SEVERE, ss.str().c_str());
+		exit(2);
+	}
+}
+
+void PrintFilmInfo(const FlexImageFilm &film) {
+	std::stringstream ss;
+
+	ss << "Film Width x Height: " << film.GetXPixelCount() << "x" << film.GetYPixelCount();
+	luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+
+	u_int bufferCount = film.GetNumBufferConfigs();
+	ss.str("");
+	ss << "Buffer Config count: " << bufferCount;
+	luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+
+	for (u_int i = 0; i < bufferCount; i++) {
+		const BufferConfig &bc = film.GetBufferConfig(i);
+
+		ss.str("");
+		ss << "Buffer Config index " << i << ": type=" << bc.type <<
+				", output=" << bc.output <<
+				", postfix=" << bc.postfix;
+		luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+	}
+
+	u_int bufferGroupCount = film.GetNumBufferGroups();
+	ss.str("");
+	ss << "Buffer Group count: " << bufferGroupCount;
+	luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+
+	for (u_int i = 0; i < bufferGroupCount; i++) {
+		ss.str("");
+		ss << "Buffer Group index " << i << ": name=" << film.GetGroupName(i) <<
+				", enable=" << film.GetGroupEnable(i) <<
+				", scale=" << film.GetGroupScale(i) <<
+				", RGBScale=(" << film.GetGroupRGBScale(i) << ")" <<
+				", temperature=" << film.GetGroupTemperature(i);
+		luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+	}
+}
+
+
+double sqr(double a) {
+	return a * a;
+}
+
+bool CheckFilms(u_int bufferIndex, FlexImageFilm &refFilm, FlexImageFilm &testFilm) {
+	std::stringstream ss;
+
+	// Dade - there are several assumption here about the number of buffers, etc.
+	Buffer *refBuf = refFilm.GetBufferGroup(0).getBuffer(bufferIndex);
+	Buffer *testBuf = testFilm.GetBufferGroup(0).getBuffer(bufferIndex);
+
+	if (refBuf->xPixelCount != testBuf->xPixelCount) {
+		ss.str("");
+		ss << "Wrong film width: " << refBuf->xPixelCount << "!=" << testBuf->xPixelCount;
+		luxError(LUX_NOERROR, LUX_WARNING, ss.str().c_str());
+		return false;
+	}
+
+	if (refBuf->yPixelCount != testBuf->yPixelCount) {
+		ss.str("");
+		ss << "Wrong film height: " << refBuf->yPixelCount << "!=" << testBuf->yPixelCount;
+		luxError(LUX_NOERROR, LUX_WARNING, ss.str().c_str());
+		return false;
+	}
+
+	return true;
+}
+
+// Dade - compare 2 buffers returning the Mean Square Error
+double CompareFilmWithMSE(u_int bufferIndex, FlexImageFilm &refFilm, FlexImageFilm &testFilm) {
+	if (!CheckFilms(bufferIndex, refFilm, testFilm))
+		return INFINITY;
+
+	// Dade - there are several assumption here about the number of buffers, etc.
+	Buffer *refBuf = refFilm.GetBufferGroup(0).getBuffer(bufferIndex);
+	Buffer *testBuf = testFilm.GetBufferGroup(0).getBuffer(bufferIndex);
+
+	double mse = 0.0;
+	XYZColor refXYZ, testXYZ;
+	float refAlpha, testAlpha;
+	for (int y = 0; y < refBuf->yPixelCount; y++) {
+		for (int x = 0; x < refBuf->xPixelCount; x++) {
+			refBuf->GetData(x, y, &refXYZ, &refAlpha);
+			testBuf->GetData(x, y, &testXYZ, &testAlpha);
+
+			mse += sqr(refXYZ.c[0] - testXYZ.c[0]);
+			mse += sqr(refXYZ.c[1] - testXYZ.c[1]);
+			mse += sqr(refXYZ.c[2] - testXYZ.c[2]);
+		}
+	}
+
+	return mse / (refBuf->xPixelCount * refBuf->yPixelCount);
+}
+
+// Dade - compare 2 buffers returning the Normalized Mean Square Error
+double CompareFilmWithNormalizedMSE(u_int bufferIndex, FlexImageFilm &refFilm, FlexImageFilm &testFilm) {
+	if (!CheckFilms(bufferIndex, refFilm, testFilm))
+		return INFINITY;
+
+	// Dade - there are several assumption here about the number of buffers, etc.
+	Buffer *refBuf = refFilm.GetBufferGroup(0).getBuffer(bufferIndex);
+	Buffer *testBuf = testFilm.GetBufferGroup(0).getBuffer(bufferIndex);
+
+	double mse = 0.0;
+	XYZColor refXYZ, testXYZ;
+	float refAlpha, testAlpha;
+	for (int y = 0; y < refBuf->yPixelCount; y++) {
+		for (int x = 0; x < refBuf->xPixelCount; x++) {
+			refBuf->GetData(x, y, &refXYZ, &refAlpha);
+			testBuf->GetData(x, y, &testXYZ, &testAlpha);
+
+			float lum = fabs(refXYZ.Y());
+			if (lum > 1e-5f) {
+				mse += (sqr(refXYZ.c[0] - testXYZ.c[0]) +
+						sqr(refXYZ.c[1] - testXYZ.c[1]) +
+						sqr(refXYZ.c[2] - testXYZ.c[2])) / lum;
+			} else
+				mse += sqr(refXYZ.c[0] - testXYZ.c[0]) +
+					sqr(refXYZ.c[1] - testXYZ.c[1]) +
+					sqr(refXYZ.c[2] - testXYZ.c[2]);
+		}
+	}
+
+	return mse / (refBuf->xPixelCount * refBuf->yPixelCount);
+}
+
+enum ComparisonTypes {
+	TYPE_NORMALIZED_MSE = 0,
+	TYPE_MSE = 1
+};
+
+int main(int ac, char *av[]) {
+
+	try {
+		std::stringstream ss;
+
+		// Declare a group of options that will be
+		// allowed only on command line
+		po::options_description generic("Generic options");
+		generic.add_options()
+				("version,v", "Print version string")
+				("help,h", "Produce help message")
+				("verbosity,V", po::value< int >(), "Log output verbosity")
+				("type,t", po::value< int >(), "Select the type of comparison")
+				;
+
+		// Hidden options, will be allowed both on command line and
+		// in config file, but will not be shown to the user.
+		po::options_description hidden("Hidden options");
+		hidden.add_options()
+				("input-file", po::value< vector<string> >(), "input file")
+				;
+
+		po::options_description cmdline_options;
+		cmdline_options.add(generic).add(hidden);
+
+		po::options_description visible("Allowed options");
+		visible.add(generic);
+
+		po::positional_options_description p;
+
+		p.add("input-file", -1);
+
+		po::variables_map vm;
+		store(po::command_line_parser(ac, av).
+				options(cmdline_options).positional(p).run(), vm);
+
+		if (vm.count("help")) {
+			ss.str("");
+			ss << "Usage: luxcomp [options] <reference film file> <test film file>\n" << visible;
+			luxError(LUX_SYSTEM, LUX_ERROR, ss.str().c_str());
+			return 0;
+		}
+
+		if (vm.count("verbosity"))
+			luxLogFilter = vm["verbosity"].as<int>();
+
+		ss.str("");
+		ss << "Lux version " << LUX_VERSION << " of " << __DATE__ << " at " << __TIME__;
+		luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+		if (vm.count("version"))
+			return 0;
+
+		ComparisonTypes compType = TYPE_NORMALIZED_MSE;
+		if (vm.count("type"))
+			compType = (ComparisonTypes)vm["type"].as<int>();
+		ss.str("");
+		ss << "Comparison type: " << compType;
+		luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+
+		if (vm.count("input-file")) {
+			const std::vector<std::string> &v = vm["input-file"].as < vector<string> > ();
+
+			if ((v.size() == 1)  || (v.size() == 2)) {
+				luxError(LUX_NOERROR, LUX_INFO, "-------------------------------");
+
+				// Dade - read the reference film
+				std::string refFileName = v[0];
+				ss.str("");
+				ss << "Reference file name: '" << refFileName << "'";
+				luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+
+				// Dade - check if the file exist
+				CheckFilePath(refFileName);
+
+				// Dade - read the reference film from the file
+				boost::scoped_ptr<FlexImageFilm> refFilm;
+				refFilm.reset((FlexImageFilm *)FlexImageFilm::CreateFilmFromFLM(refFileName));
+				if (!refFilm) {
+					ss.str("");
+					ss << "Error reading reference FLM file '" << refFileName << "'";
+					luxError(LUX_NOFILE, LUX_SEVERE, ss.str().c_str());
+					return 3;
+				}
+
+				PrintFilmInfo(*refFilm);
+
+				if (v.size() == 2) {
+					luxError(LUX_NOERROR, LUX_INFO, "-------------------------------");
+
+					// Dade - read the film to test
+					std::string testFileName = v[1];
+					ss.str("");
+					ss << "Test file name: '" << testFileName << "'";
+					luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+
+					// Dade - check if the file exist
+					CheckFilePath(testFileName);
+
+					// Dade - read the test film from the file
+					boost::scoped_ptr<FlexImageFilm> testFilm;
+					testFilm.reset((FlexImageFilm *)FlexImageFilm::CreateFilmFromFLM(testFileName));
+					if (!testFilm) {
+						ss.str("");
+						ss << "Error reading test FLM file '" << testFilm << "'";
+						luxError(LUX_NOFILE, LUX_SEVERE, ss.str().c_str());
+						return 3;
+					}
+
+					PrintFilmInfo(*testFilm);
+
+					luxError(LUX_NOERROR, LUX_INFO, "-------------------------------");
+
+					float result = 0.0f;
+					for (u_int i = 0; i < refFilm->GetNumBufferConfigs(); i++) {
+						float bufResult = 0.0f;
+
+						switch (compType) {
+							default:
+							case TYPE_NORMALIZED_MSE:
+								bufResult = CompareFilmWithNormalizedMSE(
+										i, *refFilm, *testFilm);
+								break;
+							case TYPE_MSE:
+								bufResult = CompareFilmWithMSE(i, *refFilm, *testFilm);
+								break;
+						}
+
+						result = max(bufResult, result);
+						ss.str("");
+						ss << "Result buffer index " << i << ": " << bufResult <<
+								" (" << result << ")";
+						luxError(LUX_NOERROR, LUX_INFO, ss.str().c_str());
+
+					}
+					std::cout << result << std::endl;
+				}
+			} else {
+				ss.str("");
+				ss << "luxcomp: wrong input files count";
+				luxError(LUX_SYSTEM, LUX_ERROR, ss.str().c_str());
+			}
+		} else {
+			ss.str("");
+			ss << "luxcomp: missing input files";
+			luxError(LUX_SYSTEM, LUX_ERROR, ss.str().c_str());
+		}
+	} catch (std::exception & e) {
+		std::stringstream ss;
+		ss << "Command line argument parsing failed with error '" << e.what() << "', please use the --help option to view the allowed syntax.";
+		luxError(LUX_SYNTAX, LUX_SEVERE, ss.str().c_str());
+		return 1;
+	}
+
+	return 0;
+}
