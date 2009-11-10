86f589b8777a3faa1bca0c1efc41046e117bcc4b
Jean-Philippe Grimaldi <jeanphi@via.ecp.fr>
2009-11-10 21:37:04 +0100

    Portals fixes to sky.
    Jeanphi

--- lights/sun.cpp
+++ lights/sun.cpp
@@ -318,10 +318,9 @@ bool SunLight::Sample_L(const TsPack *tspack, const Scene *scene, float u1, floa
 	} else  {
 		// Choose a random portal
 		int shapeIndex = 0;
-		if(nrPortalShapes > 1) {
-			shapeIndex = min(nrPortalShapes - 1,
-				Floor2Int(u3 * nrPortalShapes));
+		if (nrPortalShapes > 1) {
 			u3 *= nrPortalShapes;
+			shapeIndex = min(nrPortalShapes - 1, Floor2Int(u3));
 			u3 -= shapeIndex;
 		}
 
