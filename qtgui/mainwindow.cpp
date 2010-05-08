c507270d7760bb3e03fc9d6d50019dd4d4202bca
jensverwiebe <jensverwiebe@jens-macpro.fritz.box>
2010-05-08 16:48:54 +0200

    resethelper for gamma

--- qtgui/mainwindow.cpp
+++ qtgui/mainwindow.cpp
@@ -1285,6 +1285,7 @@ void MainWindow::loadTimeout()
 		indicateActivity(false);
 		statusMessage->setText("");
 		m_loadTimer->stop();
+		gammawidget->resetFromFilm(true);
 
 		if (luxStatistics("sceneIsReady")) {
 			// Scene file loaded
