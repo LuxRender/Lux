792e128078e461265fffc3731e82efe81c92bed3
Jean-Philippe Grimaldi <jeanphi@via.ecp.fr>
2010-09-21 17:35:44 +0200

    Updating "glossy" material roughness clamping as per issue #833.
    Jeanphi

--- materials/glossy2.cpp
+++ materials/glossy2.cpp
@@ -54,8 +54,8 @@ BSDF *Glossy2::GetBSDF(MemoryArena &arena, const SpectrumWavelengths &sw,
 	SWCSpectrum a(Ka->Evaluate(sw, dgs).Clamp(0.f, 1.f));
 
 	// Clamp roughness values to avoid artifacts with too small values
-	const float u = Clamp(nu->Evaluate(sw, dgs), 1e-4f, 1.f);
-	const float v = Clamp(nv->Evaluate(sw, dgs), 1e-4f, 1.f);
+	const float u = Clamp(nu->Evaluate(sw, dgs), 6e-3f, 1.f);
+	const float v = Clamp(nv->Evaluate(sw, dgs), 6e-3f, 1.f);
 	const float u2 = u * u;
 	const float v2 = v * v;
 	float ld = depth->Evaluate(sw, dgs);
