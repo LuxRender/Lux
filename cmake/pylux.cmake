dfb9a824e358fc0513a5d02e29b1fcfec153e05c
Amir Mohammadkhani-Aminabadi <amir@mohammadkhani.eu>
2011-09-08 01:50:37 +0200

    Fixed post build issues with pylux on windows. Added my cmake configuration specialization

--- cmake/pylux.cmake
+++ cmake/pylux.cmake
@@ -79,23 +79,39 @@ IF(PYTHONLIBS_FOUND OR PYTHON_CUSTOM)
 			COMMAND cp ${CMAKE_SOURCE_DIR}/python/pyluxconsole.py release/pyluxconsole.py
 		)
 	ELSE(APPLE)
+
 		TARGET_LINK_LIBRARIES(pylux ${LUX_LIBRARY} ${CMAKE_THREAD_LIBS_INIT} ${LUX_LIBRARY_DEPENDS} ${PYTHON_LIBRARIES} ${Boost_python_LIBRARIES})
-		IF(CYGWIN)
-			ADD_CUSTOM_COMMAND(
-				TARGET pylux POST_BUILD
-				COMMAND mv cygpylux.dll pylux.dll
-			)
-		ELSE(CYGWIN)
+		
+		IF (WIN32)
+			IF(CYGWIN)
+				ADD_CUSTOM_COMMAND(
+					TARGET pylux POST_BUILD
+					COMMAND mv cygpylux.dll pylux.dll
+				)
+			ENDIF(CYGWIN)
+
+			IF (MSVC)
+				# Output .pyd files for direct blender plugin usage
+				SET_TARGET_PROPERTIES(pylux
+					PROPERTIES
+					SUFFIX ".pyd"
+				)
+			ENDIF(MSVC)
+			
+		ELSE(WIN32)
+
 			ADD_CUSTOM_COMMAND(
 				TARGET pylux POST_BUILD
 				COMMAND mv libpylux.so pylux.so
 			)
-		ENDIF(CYGWIN)
-		ADD_CUSTOM_COMMAND(
-			TARGET pylux POST_BUILD
-			COMMAND cp ${CMAKE_SOURCE_DIR}/python/pyluxconsole.py pyluxconsole.py
-		)
+
+			ADD_CUSTOM_COMMAND(
+				TARGET pylux POST_BUILD
+				COMMAND cp ${CMAKE_SOURCE_DIR}/python/pyluxconsole.py pyluxconsole.py
+			)
+		ENDIF(WIN32)
+
 	ENDIF(APPLE)
 ELSE(PYTHONLIBS_FOUND OR PYTHON_CUSTOM)
-	MESSAGE( STATUS "Warning : could not find Python libraries - not building python module")
+	MESSAGE( STATUS "Warning: could not find Python libraries - not building python module")
 ENDIF(PYTHONLIBS_FOUND OR PYTHON_CUSTOM)
