1b5488f6f46c9a6c475d598007c746dbd14ad18c
Jens Verwiebe <devnull@localhost>
2012-02-04 11:41:14 +0100

    OSX: little logical refinement in cmake

--- cmake/pylux.cmake
+++ cmake/pylux.cmake
@@ -24,8 +24,8 @@
 IF(APPLE)
 	IF(OSX_OPTION_PYLUX)
 		# use Blender python libs for static compiling !
-		SET(PYTHON_LIBRARIES ${CMAKE_SOURCE_DIR}/../macos/lib/BF_pythonlibs/py32_uni_intel/libbf_python_ext.a ${CMAKE_SOURCE_DIR}/../macos/lib/BF_pythonlibs/py32_uni_intel/libbf_python.a)
-		SET(PYTHON_INCLUDE_PATH ${CMAKE_SOURCE_DIR}/../macos/include/Python3.2)
+		SET(PYTHON_LIBRARIES ${OSX_DEPENDENCY_ROOT}/lib/BF_pythonlibs/py32_uni_intel/libbf_python_ext.a ${OSX_DEPENDENCY_ROOT}/lib/BF_pythonlibs/py32_uni_intel/libbf_python.a)
+		SET(PYTHON_INCLUDE_PATH ${OSX_DEPENDENCY_ROOT}/include/Python3.2)
 		SET(PYTHONLIBS_FOUND ON)
 	ELSE(OSX_OPTION_PYLUX)
 		# compile pylux for genral purpose against Python framework
