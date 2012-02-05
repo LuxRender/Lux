80a6e62ce69f4ea00aff3bff3ab44aa4e108f0fb
Jens Verwiebe <devnull@localhost>
2012-02-05 15:53:35 +0100

    OSX: cleanup more overhead by not longer using -all_load in corelib, but link explicit binary deps into binary

--- cmake/luxcomp.cmake
+++ cmake/luxcomp.cmake
@@ -23,7 +23,7 @@ SOURCE_GROUP("Source Files\\Tools" FILES tools/luxcomp.cpp)
 ADD_EXECUTABLE(luxcomp tools/luxcomp.cpp)
 IF(APPLE)
 	add_dependencies(luxcomp luxShared) # explicitly say that the target depends on corelib build first
-	TARGET_LINK_LIBRARIES(luxcomp ${OSX_SHARED_CORELIB} ${CMAKE_THREAD_LIBS_INIT})
+	TARGET_LINK_LIBRARIES(luxcomp ${OSX_SHARED_CORELIB} ${CMAKE_THREAD_LIBS_INIT} ${Boost_LIBRARIES})
 ELSE(APPLE)
 	TARGET_LINK_LIBRARIES(luxcomp ${LUX_LIBRARY} ${CMAKE_THREAD_LIBS_INIT} ${LUX_LIBRARY_DEPENDS})
 ENDIF(APPLE)
--- cmake/pylux.cmake
+++ cmake/pylux.cmake
@@ -70,7 +70,7 @@ IF(PYTHONLIBS_FOUND OR PYTHON_CUSTOM)
 	IF(APPLE)
 		SET_TARGET_PROPERTIES(pylux PROPERTIES XCODE_ATTRIBUTE_DEPLOYMENT_POSTPROCESSING NO) # exclude pylux from strip, not possible with external symbols !
 		add_dependencies(pylux luxShared) # explicitly say that the target depends on corelib build first
-		TARGET_LINK_LIBRARIES(pylux -Wl,-undefined -Wl,dynamic_lookup ${OSX_SHARED_CORELIB} ${CMAKE_THREAD_LIBS_INIT} ${PYTHON_LIBRARIES} ${Boost_python_LIBRARIES})
+		TARGET_LINK_LIBRARIES(pylux -Wl,-undefined -Wl,dynamic_lookup ${OSX_SHARED_CORELIB} ${CMAKE_THREAD_LIBS_INIT} ${PYTHON_LIBRARIES} ${Boost_python_LIBRARIES} ${Boost_LIBRARIES})
 		ADD_CUSTOM_COMMAND(
 			TARGET pylux POST_BUILD
 			COMMAND mv release/libpylux.so release/pylux.so
