20737d0539c8daaccceea4a3f681a14f2223bf8e
Jens Verwiebe <devnull@localhost>
2012-06-04 10:23:46 +0200

    Fix little typo in recent commit

--- qtgui/mainwindow.hxx
+++ qtgui/mainwindow.hxx
@@ -237,7 +237,7 @@ public:
 	void showZoomfactor();
 	void renderScenefile(const QString& sceneFilename, const QString& flmFilename = "");
 	void renderNewScenefile(const QString& sceneFilename, const QString& flmFilename = "");
-	void openQueueFile(const QString& sceneFilename);
+	void openQueueFile(const QString& fileName);
 	void changeRenderState (LuxGuiRenderState state);
 	void endRenderingSession(bool abort = true);
 	
