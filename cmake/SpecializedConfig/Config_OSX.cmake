ab60291abe72e85688c43e2431a9b4f5f1b19cb0
Jens Verwiebe <info@jensverwiebe.de>
2014-02-13 20:18:49 +0100

    Add the Config_OSX after checking over

---
+++ cmake/SpecializedConfig/Config_OSX.cmake
@@ -0,0 +1,26 @@
+
+###########################################################################
+#
+# Configuration ( Jens Verwiebe )
+#
+###########################################################################
+
+MESSAGE(STATUS "Using OSX Configuration settings")
+
+SET(FREEIMAGE_ROOT ${OSX_DEPENDENCY_ROOT})
+set(OPENIMAGEIO_ROOT_DIR ${OSX_DEPENDENCY_ROOT})
+set(OPENEXR_ROOT ${OSX_DEPENDENCY_ROOT})
+set(PNG_ROOT ${OSX_DEPENDENCY_ROOT})
+
+set(TIFF_LIBRARIES ${OSX_DEPENDENCY_ROOT}/lib/libtiff.a)
+set(TIFF_INCLUDE_DIR ${OSX_DEPENDENCY_ROOT}/include/tiff)
+include_directories(SYSTEM ${TIFF_INCLUDE_DIR})
+set(TIFF_FOUND ON)
+set(JPEG_LIBRARIES ${OSX_DEPENDENCY_ROOT}/lib/libjpeg.a)
+set(JPEG_INCLUDE_DIR ${OSX_DEPENDENCY_ROOT}/include/jpeg)
+include_directories(SYSTEM ${JPEG_INCLUDE_DIR})
+set(JPEG_FOUND ON)
+SET(PNG_LIBRARIES ${OSX_DEPENDENCY_ROOT}/lib/libpng14.a)
+SET(PNG_INCLUDE_DIR ${OSX_DEPENDENCY_ROOT}/include/png)
+include_directories(SYSTEM ${PNG_INCLUDE_DIR})
+SET(PNG_FOUND ON)
