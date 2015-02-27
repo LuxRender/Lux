5fbf46ff2b2d8b34d17aa22c23cfae4388904a38
Jens Verwiebe <info@jensverwiebe.de>
2015-02-28 00:03:18 +0100

    Whitespace killed in last commit

--- renderers/luxcorerenderer.cpp
+++ renderers/luxcorerenderer.cpp
@@ -1287,7 +1287,7 @@ template<class T> string GetLuxCoreTexName(luxcore::Scene *lcScene,
 		} else {
 			LOG(LUX_WARNING, LUX_UNIMPLEMENT) << "LuxCoreRenderer supports only ImageSpectrumTexture, ImageFloatTexture, ConstantRGBColorTexture, "
 					"ConstantFloatTexture, ScaleTexture, MixTexture, Checkerboard2D, Checkerboard3D, FBmTexture, MarbleTexture, DotsTexture,"
-					" BrickTexture, AddTexture, SubtractTexture, WindyTexture, WrinkledTexture, UVTexture, BandTexture, HitPointRGBColorTexture, "
+					"BrickTexture, AddTexture, SubtractTexture, WindyTexture, WrinkledTexture, UVTexture, BandTexture, HitPointRGBColorTexture, "
 					"HitPointAlphaTexture, HitPointGreyTexture, BlenderBlendTexture3D, BlenderCloudsTexture3D, BlenderDistortedNoiseTexture3D, "
 					"BlenderMagicTexture3D, BlenderMarbleTexture3D, BlenderMusgraveTexture3D, BlenderVoronoiTexture3D and BlenderWoodTexture3D "
 					"(i.e. not " << ToClassName(tex) << ").";
