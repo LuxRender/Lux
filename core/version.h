8253268ffde32be9fd813ac2cbd3aa2874cb2590
Paolo Ciccone <phciccone@gmail.com>
2015-07-07 07:39:06 -0700

    Reverted to previous version to restore build system

--- core/version.h
+++ core/version.h
@@ -30,6 +30,7 @@
 #define LUX_VN_MAJOR 1
 #define LUX_VN_MINOR 5
 #define LUX_VN_PATCH 0
+#define LUX_VN_BUILD 0
 #define LUX_VN_LABEL "RC1"
 
 #define LUX_SERVER_PROTOCOL_VERSION  1011
@@ -37,7 +38,8 @@
 #define LUX_VERSION_STRING           VERSION_STR(LUX_VN_MAJOR)     \
                                      "." VERSION_STR(LUX_VN_MINOR) \
                                      "." VERSION_STR(LUX_VN_PATCH) \
-                                     " " LUX_VN_LABEL
+                                     " " LUX_VN_LABEL \
+                                     " Build " VERSION_STR(LUX_VN_BUILD)
 
 //! Renderfarms rely on the 'protocol' part of in server version string
 #define LUX_SERVER_VERSION_STRING    LUX_VERSION_STRING " (protocol: " VERSION_STR(LUX_SERVER_PROTOCOL_VERSION) ")"
